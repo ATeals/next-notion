0a7bb998d87c52968ce8abdfb5c621a5e03de441
ateals <ateals@icloud.com>
2023-11-15 17:20:43 +0900

    change

--- src/app/posts/[id]/page.tsx
+++ src/app/posts/[id]/page.tsx
@@ -1,14 +1,21 @@
 import MDXComponent from "@/components/MDXComponent";
 import { notionPostData } from "@/util/notion";
+import getPostData from "@/util/notion/posts/getPostData";
+import { Client } from "@notionhq/client";
 
 export default async ({ params: { id } }: { params: { id: string } }) => {
+  // const parent = await notionPostData(id);
+
   const start = performance.now();
-  const parent = await notionPostData(id);
+  const notion = new Client({ auth: process.env.NOTION_KEY });
+  const res = await getPostData(notion, id);
 
   console.log(performance.now() - start);
+
   return (
     <main className="p-4">
-      <MDXComponent source={parent} />
+      <MDXComponent source={res} />
+      Post
     </main>
   );
 };
