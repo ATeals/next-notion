ab32e8c8c7ca2217d19dbbbe0923ea9043a7e9e5
ateals <ateals@icloud.com>
2023-11-15 17:01:08 +0900

    performance check

--- src/app/posts/[id]/page.tsx
+++ src/app/posts/[id]/page.tsx
@@ -1,11 +1,11 @@
 import MDXComponent from "@/components/MDXComponent";
 import { notionPostData } from "@/util/notion";
 
-export const runtime = "edge";
-
 export default async ({ params: { id } }: { params: { id: string } }) => {
+  const start = performance.now();
   const parent = await notionPostData(id);
 
+  console.log(performance.now() - start);
   return (
     <main className="p-4">
       <MDXComponent source={parent} />
