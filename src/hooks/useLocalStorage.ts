fc56b5107dd7068cdbca586fa1ac766ba9caa6f0
ateals <ateals@icloud.com>
2023-11-23 14:01:16 +0900

    feat: useLocalStorage

---
+++ src/hooks/useLocalStorage.ts
@@ -0,0 +1,30 @@
+import { useSyncExternalStore } from "react";
+
+const subscribe = (listener: () => void) => {
+  window.addEventListener("storage", listener);
+  return () => window.removeEventListener("storage", listener);
+};
+
+function useLocalStorage<T>(
+  key: string,
+  initialValue?: T
+): [T, (dispatch: (value: T) => T | T) => void] {
+  const getSnapShot = (): T => {
+    const value = window.localStorage.getItem(key);
+
+    return value !== null ? JSON.parse(value) : initialValue;
+  };
+
+  const localStorageValue = useSyncExternalStore(subscribe, getSnapShot);
+
+  const setLocalStorage = (dispatch: (value: T) => T | T): void => {
+    const state = typeof dispatch === "function" ? dispatch(localStorageValue) : dispatch;
+
+    window.localStorage.setItem(key, JSON.stringify(state));
+    window.dispatchEvent(new StorageEvent("storage"));
+  };
+
+  return [localStorageValue, setLocalStorage];
+}
+
+export default useLocalStorage;
