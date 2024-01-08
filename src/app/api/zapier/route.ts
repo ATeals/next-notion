37f85367210a7ef2b0553503ece93881286e7d7a
ateals <ateals@icloud.com>
2024-01-08 20:30:34 +0900

    test: zapier req

--- src/app/api/zapier/route.ts
+++ src/app/api/zapier/route.ts
@@ -1,7 +1,7 @@
 import { NextRequest, NextResponse } from "next/server";
 
 export async function POST(request: NextRequest) {
-  const req = request.json();
+  const req = await request.json();
 
   console.log(req);
 
