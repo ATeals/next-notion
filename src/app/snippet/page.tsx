12c1d30469d44032d0fc6afea882286cafb159d3
ateals <ateals@icloud.com>
2024-01-13 23:03:22 +0900

    style: snippet page

--- src/app/snippet/page.tsx
+++ src/app/snippet/page.tsx
@@ -2,6 +2,7 @@ import { notionPosts } from "@/notion";
 
 import Link from "next/link";
 import { TagList } from "../tags/@tagList/TagList";
+import { generateClassName } from "@/util/generateClassName";
 
 export default async () => {
   const posts = await notionPosts({
@@ -17,29 +18,32 @@ export default async () => {
   return (
     <section className="dark:text-white mt-[80px] max-w-[680px] m-auto">
       <h1 className="font-bold text-4xl my-5 px-10 md:px-20">Snippet</h1>
-      <section className="md:max-m-10 m-auto px-10 md:px-20">
+      <section className="md:max-m-10 m-auto px-10 md:px-20 group/list">
         {posts.map((post) => (
           <article className=" my-5 group">
             <Link href={`/posts/${post?.id}`} key={post?.id}>
-              <div className="group-hover:underline flex justify-between">
-                <span className="lg:text-lg">{post?.title}</span>
-                <span className="text-gray-400">{post?.createdAt}</span>
+              <div className=" group-hover:underline flex justify-between group-hover/list:text-gray-200 dark:group-hover/list:text-gray-700">
+                <span className="lg:text-lg group-hover:text-black dark:group-hover:text-white">
+                  {post?.title}
+                </span>
+                <span className="group-hover:text-black dark:group-hover:text-white">
+                  {post?.createdAt}
+                </span>
               </div>
-              <p className="hidden group-hover:block animate-fadeIn text-sm lg:text-md text-gray-400 my-2">
-                {post?.description}
-              </p>
+              <div
+                className={generateClassName(
+                  "max-h-0 overflow-hidden text-xs transition-[max-height] duration-500 group-hover:max-h-10",
+                  "text-sm lg:text-md text-gray-400 my-2"
+                )}
+              >
+                <p>{post?.description}</p>
+                <div className="hidden group-hover:block mt-2">
+                  {post?.tags?.map((tag) => (
+                    <span>#{tag.name}</span>
+                  ))}
+                </div>
+              </div>
             </Link>
-            <div className="hidden group-hover:block animate-fadeIn">
-              {post?.tags?.map((tag) => (
-                <Link
-                  href={`/tags/${tag.name}`}
-                  key={tag.id + post.id}
-                  className="hover:underline cursor-pointer mr-2 text-gray-400 text-sm lg:text-md"
-                >
-                  #{tag.name}
-                </Link>
-              ))}
-            </div>
           </article>
         ))}
       </section>
