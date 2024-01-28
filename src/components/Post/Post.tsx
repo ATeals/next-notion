56911e775d4209120efd553da92e346d82b8b334
ateals <ateals@icloud.com>
2024-01-28 17:06:29 +0900

    style: post hover
    포스트 카드 호버시 이미지 스케일 업 + 그림자

--- src/components/Post/Post.tsx
+++ src/components/Post/Post.tsx
@@ -8,7 +8,7 @@ export const Post = ({ post }: { post: PostInfo }) => {
   return (
     <div key={post.id} className="group">
       <Link href={`/posts/${post.id}`}>
-        <div className=" overflow-hidden rounded-lg mb-5">
+        <div className=" overflow-hidden rounded-lg mb-5 group-hover:scale-105 duration-200 group-hover:shadow-xl">
           <div
             className="w-full h-[200px] bg-cover rounded-lg  group-hover:scale-110 transition duration-300 ease-in-out"
             style={{
