c3a40aeaf199b0b8edf85bc1690bbab31793b527
ateals <ateals@icloud.com>
2024-02-13 18:21:57 +0900

    fix: post링크는 루트 바로 뒤에 와야합니다.

--- src/feature/post/components/PostGrid.tsx
+++ src/feature/post/components/PostGrid.tsx
@@ -10,11 +10,11 @@ export const PostGrid = ({ posts }: { posts: Array<PostInfo> }) => {
       <section className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-10 text-[1rem] flex-col">
         {posts?.map((post) => (
           <PostCard key={post.id} post={post}>
-            <Link href={`posts/${post.id}`}>
+            <Link href={`/posts/${post.id}`}>
               <PostCard.Image />
             </Link>
             <PostCard.Tags />
-            <Link href={`posts/${post.id}`}>
+            <Link href={`/posts/${post.id}`}>
               <PostCard.Title />
               <PostCard.Description />
               <PostCard.TimeLine />
