0c0b63e96b78a8570382f950dcb155e10f7e078b
ateals <ateals@icloud.com>
2024-03-03 22:10:34 +0900

    refactor: error page

--- src/app/posts/[id]/error.tsx
+++ src/app/posts/[id]/error.tsx
@@ -1,17 +1,9 @@
 "use client";
 
-import { useRouter } from "next/navigation";
-
 export default () => {
-  const router = useRouter();
-
-  const handleClick = () => {
-    router.refresh();
-  };
   return (
     <section className="flex justify-center items-center flex-col pt-[100px]">
-      <h2>포스트를 불러오던 중 오류가 발생했습니다.</h2>
-      <button onClick={handleClick}>새로고침</button>
+      <h2 className="mb-10">포스트를 불러오던 중 오류가 발생했습니다.</h2>
     </section>
   );
 };
