b7fd04f43648368d0ca109daa5bcd5e7a0493a05
ateals <ateals@icloud.com>
2024-04-03 22:55:54 +0900

    feat(#10): ui - 기본 세팅 + tailwind 색 + 에니메이션 설정

--- packages/ui/src/App.tsx
+++ packages/ui/src/App.tsx
@@ -1,5 +1,41 @@
+import { generateClassName } from "@repo/utils";
+
 const App = () => {
-  return <h1>ui</h1>;
+  return (
+    <div className="w-full h-screen dark:bg-black-bg">
+      <Page />
+    </div>
+  );
 };
 
 export default App;
+
+const Box = ({ className }: { className: string }) => {
+  const tailwind = generateClassName(
+    "w-40 h-40 rounded-lg m-2 shadow-lg dark:shadow-zinc-900",
+    className
+  );
+
+  return <div className={tailwind}></div>;
+};
+
+const Page = () => {
+  return (
+    <>
+      <h1>hello</h1>
+      <div className="flex animate-fadeInDown">
+        <Box className="bg-primary-xl" />
+        <Box className="bg-primary-lg" />
+        <Box className="bg-primary-md" />
+        <Box className="bg-primary-sm" />
+      </div>
+
+      <div className="flex animate-[fadeIn_1s_200ms]">
+        <Box className="bg-secondary-xl" />
+        <Box className="bg-secondary-lg" />
+        <Box className="bg-secondary-md" />
+        <Box className="bg-secondary-sm" />
+      </div>
+    </>
+  );
+};
