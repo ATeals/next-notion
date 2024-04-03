cc64e4a1c117a3757d7857160f447ac86777ebe3
ateals <ateals@icloud.com>
2024-04-04 00:05:05 +0900

    feat(#10): ui -  dividerline

---
+++ packages/ui/src/atom/DividerLine/DividerLine.tsx
@@ -0,0 +1,21 @@
+import { PolymorphicComponentProps } from "@/types/PolymorphicComponentProps";
+import { generateClassName } from "@repo/utils";
+
+type DividerLineProps = {
+  direction?: "horizontal" | "vertical";
+};
+
+export const DividerLine = ({
+  direction = "horizontal",
+  className,
+  ...props
+}: PolymorphicComponentProps<"div", DividerLineProps>) => {
+  const tailwind = generateClassName(
+    "border-gray-300",
+    direction === "horizontal" && `border-b-2 w-full`,
+    direction === "vertical" && `border-l-2 h-full`,
+    className
+  );
+
+  return <div className={tailwind} {...props} />;
+};
