3bc9cc8dfe7c444adaf7d166ac199d300d3ac08e
ateals <ateals@icloud.com>
2024-04-04 00:05:32 +0900

    feat(#10): ui -Grid

---
+++ packages/ui/src/atom/Grid/Container/GridContainer.tsx
@@ -0,0 +1,31 @@
+import { GridContainerStyles, getGridContainerStyles } from "./GridContainer.styles";
+import { PolymorphicComponentProps } from "@/types/PolymorphicComponentProps";
+import { AsChildProps, Slot } from "@/common/Slot";
+import { generateClassName } from "@repo/utils";
+
+type GridContainerProps = {
+  style?: GridContainerStyles & React.CSSProperties;
+} & AsChildProps;
+
+export const GridContainer = <T extends React.ElementType = "div">({
+  as,
+  style = {},
+  asChild,
+  ...props
+}: PolymorphicComponentProps<T, GridContainerProps>) => {
+  const Element = asChild ? Slot : as || "div";
+
+  const tailwind = generateClassName(props.className);
+
+  return (
+    <Element
+      className={tailwind}
+      style={{
+        ...getGridContainerStyles(style),
+      }}
+      {...props}
+    >
+      {props.children}
+    </Element>
+  );
+};
