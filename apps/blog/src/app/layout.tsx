6f0e2475d710024b602f9139fa499daa568b0101
ateals <ateals@icloud.com>
2024-04-04 00:08:48 +0900

    chore: bootstrap icon

--- apps/blog/src/app/layout.tsx
+++ apps/blog/src/app/layout.tsx
@@ -7,6 +7,14 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="ko">
+      <head>
+        <link
+          rel="stylesheet"
+          href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.1/font/bootstrap-icons.css"
+          integrity="sha384-4LISF5TTJX/fLmGSxO53rV4miRxdg84mZsxmO8Rx5jGtp/LbrixFETvWa5a6sESd"
+          crossOrigin="anonymous"
+        />
+      </head>
       <body>{children}</body>
     </html>
   );
