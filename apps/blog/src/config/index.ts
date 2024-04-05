8c1787e0990a779badd7415c787f653f386b3533
ateals <ateals@icloud.com>
2024-04-06 01:07:15 +0900

    feat(#10): BLOG config

--- apps/blog/src/config/index.ts
+++ apps/blog/src/config/index.ts
@@ -2,6 +2,27 @@ export const DEFAULT_PAGE_WIDTH = "700px";
 
 export const DEFAULT_HEADER_HEIGHT = "46px";
 
+export const MAIN_WEBP = "/images/main.webp";
+
 export const BLOG_CONFIG = {
   LOGO: "/images/logo.webp",
+  PATH: {
+    HOME: "/",
+    SNIPPETS: "/snippets",
+    TAGS: "/tags",
+    POSTS: "/posts",
+  },
+  REVALIDATE_TAGNAME: "REVALIDATE_TAG",
+};
+
+export const SITE_CONFIG = {
+  title: "Teals",
+  description: "Teal의 개발 블로그",
+  url: "https://ateals.vercel.app/",
+  since: "2023",
+  owner: {
+    name: "ateals",
+    email: "ateals@icloud.com",
+    link: "https://ateals.vercel.app/",
+  },
 };
