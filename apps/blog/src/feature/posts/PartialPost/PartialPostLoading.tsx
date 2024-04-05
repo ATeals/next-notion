3b410bc2469c681c1c1527f14c4e691b9058e755
ateals <ateals@icloud.com>
2024-04-06 00:59:34 +0900

    feat(#10): RecursivePartialPost

---
+++ apps/blog/src/feature/posts/PartialPost/PartialPostLoading.tsx
@@ -0,0 +1,7 @@
+"use client";
+
+export const PartialPostLoading = () => {
+  return (
+    <div className="bg-opacity-20  bg-gray-500 animate-pulse rounded-md p-5 m-1 h-40 my-10 flex justify-center items-center dark:text-white"></div>
+  );
+};
