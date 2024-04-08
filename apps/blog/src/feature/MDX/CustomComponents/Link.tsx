4da5b0daa11161f4a647a5cdfd1517b472846a89
ateals <ateals@icloud.com>
2024-04-08 23:56:08 +0900

    fix: bookmark style

--- apps/blog/src/feature/MDX/CustomComponents/Link.tsx
+++ apps/blog/src/feature/MDX/CustomComponents/Link.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { SITE_CONFIG } from "@/config";
+import { BLOG_CONFIG, SITE_CONFIG } from "@/config";
 import { ErrorBoundary } from "next/dist/client/components/error-boundary";
 
 export interface OpenGraph {
@@ -21,35 +21,32 @@ export const a = ({ href, children, ...props }: React.AnchorHTMLAttributes<HTMLA
 };
 
 const BookMark = async ({ href }: { href: string | undefined }) => {
-  const BASE_URL =
-    process.env.NODE_ENV === "production" ? SITE_CONFIG.url : "http://localhost:3000/";
-
-  const og: OpenGraph = await (await fetch(`${BASE_URL}api/og?url=${href}`)).json();
+  const og: OpenGraph = await (await fetch(`${BLOG_CONFIG.DOMAIN}/api/og?url=${href}`)).json();
 
   const { title, url, img, description } = og;
 
-  const isFullOg = title !== undefined && img !== undefined;
+  const isValidOg = title !== undefined && img !== undefined;
 
-  return (
-    isFullOg && (
-      <a
-        href={href}
-        target="_blank"
-        className="my-10 block lg:flex shadow-md hover:scale-105 group no-underline lg:max-h-[200px]"
-      >
-        <span className="block w-full lg:w-[40%] ">
-          <img src={img} alt="ogImage" className="object-cover w-full h-full m-0" />
+  return isValidOg ? (
+    <a
+      href={href}
+      target="_blank"
+      className="my-10 block md:flex shadow-md hover:scale-105 group no-underline md:max-h-[200px] transition-all duration-200 ease-in-out"
+    >
+      <span className="block w-full md:w-[40%] h-[200px] md:h-auto">
+        <img src={img} alt="ogImage" className="object-contain md:object-cover w-full h-full m-0" />
+      </span>
+      <span className="flex flex-col justify-center p-5 md:w-[60%]">
+        <span className="block mb-5">{title}</span>
+        <span className="block text-sm text-gray-400 mb-5 overflow-clip">
+          {description.length > 125 ? description.slice(1, 125) + "..." : description}
         </span>
-        <span className="flex flex-col justify-center p-5 lg:w-[60%]">
-          <span className="block mb-5">{title}</span>
-          <span className="block text-sm text-gray-400 mb-5 overflow-clip">
-            {description.length > 125 ? description.slice(1, 125) + "..." : description}
-          </span>
-          <span className="lg:text-end text-sm text-gray-400 overflow-hidden overflow-ellipsis max-h-[1.4rem]">
-            {url}
-          </span>
+        <span className="group-hover:text-secondary-lg md:text-end text-sm text-gray-400 overflow-hidden overflow-ellipsis max-h-[1.4rem]">
+          {url}
         </span>
-      </a>
-    )
+      </span>
+    </a>
+  ) : (
+    <a href={href}> {href}</a>
   );
 };
