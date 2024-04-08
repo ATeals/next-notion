94fe53ddece81db6917128bcc037334eadbf7994
ateals <ateals@icloud.com>
2024-04-09 00:56:07 +0900

    fix: ! 임시 bookmark 수정
    bookmark 컴포넌트에서 og api를 무한 호출하는 문제 발생 -> client 컴포넌트로 수정 -> Suspense 검토중

--- apps/blog/src/feature/MDX/CustomComponents/Link.tsx
+++ apps/blog/src/feature/MDX/CustomComponents/Link.tsx
@@ -1,8 +1,8 @@
 "use client";
 
-import { BLOG_CONFIG, SITE_CONFIG } from "@/config";
+import { BLOG_CONFIG } from "@/config";
 import { ErrorBoundary } from "next/dist/client/components/error-boundary";
-import { Suspense } from "react";
+import { Suspense, useEffect, useState } from "react";
 
 export interface OpenGraph {
   title: string;
@@ -12,40 +12,59 @@ export interface OpenGraph {
 }
 
 export const a = ({ href, children, ...props }: React.AnchorHTMLAttributes<HTMLAnchorElement>) => {
-  return children === "bookmark" ? (
+  return children === "bookmark" && href ? (
     <ErrorBoundary errorComponent={() => <span></span>}>
-      <Suspense fallback={<span></span>}>
-        <BookMark href={href} />
-      </Suspense>
+      <BookMark href={href} />
     </ErrorBoundary>
   ) : (
     <a href={href}> {children ? children : href}</a>
   );
 };
 
-const BookMark = async ({ href }: { href: string | undefined }) => {
-  const og: OpenGraph = await (await fetch(`${BLOG_CONFIG.DOMAIN}/api/og?url=${href}`)).json();
+const useOg = (href: string) => {
+  const [og, setOg] = useState<OpenGraph | null>(null);
+  const [loading, setLoading] = useState(true);
 
-  const { title, url, img, description } = og;
+  const fetchOg = async (href: string) => {
+    const og: OpenGraph = await (await fetch(`${BLOG_CONFIG.DOMAIN}/api/og?url=${href}`)).json();
 
-  const isValidOg = title !== undefined && img !== undefined;
+    setOg(og);
+    setLoading(false);
+  };
 
-  return isValidOg ? (
+  useEffect(() => {
+    fetchOg(href);
+  }, [href]);
+
+  return { og, loading };
+};
+
+const BookMark = ({ href }: { href: string }) => {
+  const { og, loading } = useOg(href);
+
+  if (loading)
+    return <a href={href} className="min-h-[200px] w-full bg-gray-300 animate-pulse"></a>;
+
+  return og ? (
     <a
       href={href}
       target="_blank"
       className="group/bookmark my-10 block md:flex  shadow-md hover:scale-105  no-underline md:max-h-[200px] transition-all duration-200 ease-in-out"
     >
       <span className="block w-full md:w-[40%] h-[200px] md:h-auto">
-        <img src={img} alt="ogImage" className="object-contain md:object-cover w-full h-full m-0" />
+        <img
+          src={og.img}
+          alt="ogImage"
+          className="object-contain md:object-cover w-full h-full m-0"
+        />
       </span>
       <span className="flex flex-col justify-center p-5 md:w-[60%]">
-        <span className="block mb-5">{title}</span>
+        <span className="block mb-5">{og.title}</span>
         <span className="block text-sm text-gray-400 mb-5 overflow-clip">
-          {description.length > 125 ? description.slice(1, 125) + "..." : description}
+          {og.description.length > 125 ? og.description.slice(1, 125) + "..." : og.description}
         </span>
         <span className="group-hover/bookmark:text-secondary-lg text-sm text-gray-400 overflow-hidden overflow-ellipsis max-h-[1.4rem]">
-          {url}
+          {og.url}
         </span>
       </span>
     </a>
