7f3565278e54374ddfde1c894cf3769141a187b2
ateals <ateals@icloud.com>
2024-04-10 21:38:12 +0900

    feat: Demo page

---
+++ apps/markdown-editor/src/pages/Home/index.tsx
@@ -0,0 +1,41 @@
+import { useMemo, useState } from "react";
+
+import { useParmasData } from "@/feature/common/hooks/useParamsData";
+
+import "@/styles/github-md-style.css";
+import { MarkdownViewer, MarkdownEditor } from "@/feature/Markdown";
+
+const useMarkdownInput = () => {
+  const { getData, setData } = useParmasData("");
+
+  const [input, setInput] = useState(() => getData());
+
+  const register = () => {
+    return useMemo(
+      () => ({
+        value: input,
+        onChange: (e: React.ChangeEvent<HTMLTextAreaElement>) => {
+          setInput(e.currentTarget.value);
+          setData(e.currentTarget.value);
+        },
+      }),
+      [input]
+    );
+  };
+
+  return { register, markdown: input, setMarkdown: setInput };
+};
+
+export const Home = () => {
+  const { register, markdown: md } = useMarkdownInput();
+
+  return (
+    <div className="flex h-dvh">
+      <div className="w-1/2 h-full">
+        <MarkdownEditor {...register()} />
+      </div>
+
+      <MarkdownViewer className="w-1/2">{md}</MarkdownViewer>
+    </div>
+  );
+};
