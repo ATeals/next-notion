8b1c0d0b077c5080afce1ff63696a0a8dddd72f4
ateals <ateals@icloud.com>
2024-04-11 10:45:03 +0900

    refactor: home 페이지 변경

--- apps/markdown-editor/src/pages/Home/index.tsx
+++ apps/markdown-editor/src/pages/Home/index.tsx
@@ -1,40 +1,37 @@
-import { useMemo, useState } from "react";
-
-import { useParmasData } from "@/feature/common/hooks/useParamsData";
-
-import { MarkdownViewer, MarkdownEditor } from "@/feature/Markdown";
-
-const useMarkdownInput = () => {
-  const { getData, setData } = useParmasData("");
-
-  const [input, setInput] = useState(() => getData());
-
-  const register = () => {
-    return useMemo(
-      () => ({
-        value: input,
-        onChange: (e: React.ChangeEvent<HTMLTextAreaElement>) => {
-          setInput(e.currentTarget.value);
-          setData(e.currentTarget.value);
-        },
-      }),
-      [input]
-    );
-  };
-
-  return { register, markdown: input, setMarkdown: setInput };
-};
+import { MarkdownViewer, MarkdownEditor, useMarkdownInput } from "@/feature/Markdown";
 
 export const Home = () => {
-  const { register, markdown: md } = useMarkdownInput();
+  const { register, markdown, addShortcut } = useMarkdownInput();
 
   return (
-    <div className="flex h-dvh">
-      <div className="w-1/2 h-full">
-        <MarkdownEditor {...register()} />
+    <div className="flex h-dvh p-5">
+      <div className="w-1/2 h-full rounded-lg bg-white p-5">
+        <MarkdownEditor className="rounded-lg h-full w-full" {...register()}>
+          <MarkdownEditor.Toolbar>
+            <MarkdownEditor.Trigger onClick={() => addShortcut("h1")}>h1</MarkdownEditor.Trigger>
+            <MarkdownEditor.Trigger onClick={() => addShortcut("strong")}>
+              bold
+            </MarkdownEditor.Trigger>
+            <MarkdownEditor.Trigger onClick={() => addShortcut("itelic")}>
+              itelic
+            </MarkdownEditor.Trigger>
+            <MarkdownEditor.Trigger onClick={() => addShortcut("hyperlink")}>
+              link
+            </MarkdownEditor.Trigger>
+            <MarkdownEditor.Trigger onClick={() => addShortcut("quotes")}>
+              quotes
+            </MarkdownEditor.Trigger>
+            <MarkdownEditor.Trigger onClick={() => addShortcut("img")}>img</MarkdownEditor.Trigger>
+            <MarkdownEditor.Trigger onClick={() => addShortcut("code")}>
+              code
+            </MarkdownEditor.Trigger>
+          </MarkdownEditor.Toolbar>
+        </MarkdownEditor>
       </div>
 
-      <MarkdownViewer className="w-1/2">{md}</MarkdownViewer>
+      <MarkdownViewer className="w-1/2 border-2 border-gray-300 rounded-lg overflow-scroll">
+        {markdown}
+      </MarkdownViewer>
     </div>
   );
 };
