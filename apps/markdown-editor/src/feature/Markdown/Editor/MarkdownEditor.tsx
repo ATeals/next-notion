a296922ca19fe6dac8b99e8f5749e3a8f9b07e81
ateals <ateals@icloud.com>
2024-04-11 10:44:11 +0900

    feat: MarkdownEditor Toolbar , Triger

--- apps/markdown-editor/src/feature/Markdown/Editor/MarkdownEditor.tsx
+++ apps/markdown-editor/src/feature/Markdown/Editor/MarkdownEditor.tsx
@@ -2,8 +2,53 @@ import { generateClassName } from "@/generateClassName";
 
 type MarkdownEditorProps = React.HTMLProps<HTMLTextAreaElement> & {};
 
-export const MarkdownEditor = ({ className, ...props }: MarkdownEditorProps) => {
-  const css = generateClassName("w-full p-2 resize-none h-full bg-gray-200", className);
+export const MarkdownEditorProvider = ({
+  className,
+  value,
+  onChange,
+  children,
+  ...props
+}: MarkdownEditorProps) => {
+  const css = generateClassName("relative", className);
 
-  return <textarea className={css} {...props} />;
+  return (
+    <div className={css}>
+      <textarea
+        className={"resize-none w-full h-full p-2 pt-[50px] "}
+        value={value}
+        onChange={onChange}
+        {...props}
+      ></textarea>
+      {children}
+    </div>
+  );
 };
+
+export const MarkdownEditorToolbar = ({ children }: { children: React.ReactNode }) => {
+  return (
+    <div className="flex absolute top-0 left-0 w-full border-[0.1rem] border-gray-300 rounded-lg overflow-scroll">
+      {children}
+    </div>
+  );
+};
+
+export const MarkdownEditorTrigger = ({
+  children,
+  ...props
+}: React.ButtonHTMLAttributes<HTMLButtonElement>) => {
+  const css = generateClassName(
+    "p-2 border-r-[0.1rem] border-gray-300 flex-shrink-0",
+    props.className
+  );
+
+  return (
+    <button className={css} {...props}>
+      {children}
+    </button>
+  );
+};
+
+export const MarkdownEditor = Object.assign(MarkdownEditorProvider, {
+  Toolbar: MarkdownEditorToolbar,
+  Trigger: MarkdownEditorTrigger,
+});
